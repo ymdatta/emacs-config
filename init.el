7c3b7c2cb46a75117a7f76a8a7aa4a28fc0e1d3c
ymdatta <ymdatta@protonmail.com>
2018-10-24 23:41:58 +0530

    added org-bullets package.

--- init.el
+++ init.el
@@ -22,3 +22,23 @@
 (use-package which-key
   :ensure t
   :config (which-key-mode))
+
+;; Org-mode stuff
+(use-package org-bullets
+  :ensure t
+  :config
+  (add-hook 'org-mode-hook (lambda() (org-bullets-mode 1))))
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages
+   (quote
+    (org-bullets markdown-mode which-key use-package try))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
