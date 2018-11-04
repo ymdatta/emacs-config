b21d6ecbd05394a73a7f65c871746aaf9933ffa1
ymdatta <ymdatta@protonmail.com>
2018-11-04 10:07:53 +0530

    added flycheck package.

--- init.el
+++ init.el
@@ -15,3 +15,17 @@
   (package-install 'use-package))
 
 (org-babel-load-file (expand-file-name "~/.emacs.d/myinit.org"))
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages
+   (quote
+    (auto-complete markdown-mode which-key use-package try org-bullets counsel auto-org-md))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
